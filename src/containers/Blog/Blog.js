f9ec9d717098da4ca843c85dc15d21f4500344c5
Xinran-Ma <xinran_ma@hotmail.com>
2020-05-10 21:25:40 +1000

    Added default global settings to the Axios.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,7 +14,7 @@ class Blog extends Component {
     }
 
     componentDidMount () {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 // update state within the 'then' request
                 const posts = response.data.slice(0, 4);

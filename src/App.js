b5b79a8fedd75367cc2f2a25218a3ae921a8c03b
Xinran-Ma <xinran_ma@hotmail.com>
2020-05-16 23:33:19 +1000

    Added basename to the domain settings.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      // if the domain is example.com/my-app, we need to set a basename like below. Otherwise the basename is '/'
+      <BrowserRouter basename="/my-app">
         <div className="App">
           <Blog />
         </div>
